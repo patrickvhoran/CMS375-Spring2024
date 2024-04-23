9b4c83292b2ca8577c0eaa033210b353fc25eda7
wedberry <111798361+wedberry@users.noreply.github.com>
2024-04-22 23:36:33 -0400

    Add files via upload

---
+++ admin.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Add Form</title>
+    <style>
+       
+    </style>
+</head>
+
+<body>
+    <h1>Welcome to Transportation Database</h1>
+    
+    <!-- Button to login page -->
+    <form action="insert_route.php" method="get">
+        <button type="submit">Insert Route</button>
+    </form>
+    
+    <!-- Button to create account page -->
+    <form action="insert_vehicle.php" method="get">
+        <button type="submit">Insert Vehicle</button>
+    </form>
+
+    <!-- Button to admin page -->
+    <form action="insert_station.php" method="get">
+        <button type="submit">Insert Route</button>
+    </form>
+</body>
+
+</html>
