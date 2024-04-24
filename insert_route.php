bf365a9b543261eef8f535a4b82151bf4343fcb2
wedberry <111798361+wedberry@users.noreply.github.com>
2024-04-24 13:22:36 -0400

    Ryan Final Updates

--- insert_route.php
+++ insert_route.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Add Form</title>
+    <title>Insert Route</title>
     <style>
         /* Style for the form container */
         .form-container {
@@ -302,7 +302,7 @@
         }
 
     }
-    
+
 
     // Close connection
     $conn->close();
